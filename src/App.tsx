7d92526ac224651b42e1c132d4d4f5c62977bb48
Your Name <you@example.com>
2025-02-21 09:33:48 +0100

    premier commit du projet

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Hello Bootstra avec React !</h1>
+      <button className="btn btn-success">Clique ici</button>
+    </div>
+  );
+}
+
+export default App;
